53b7c00e7fb5118edcfa39e74fba5e7855d2b7af
SimonGaufreteau <simon.gaufreteau@gmail.com>
2020-06-07 13:24:31 +0200

    Changed the GUI to accept the receiveFile method

--- src/com/tftpclient/GUITFTP.java
+++ src/com/tftpclient/GUITFTP.java
@@ -10,33 +10,53 @@ public class GUITFTP extends JPanel implements ActionListener {
 	JButton send,receive,selectFile;
 	JFileChooser fc;
 	File currentFile;
-	JTextArea log,currentFileName;
+	JTextArea log,currentFileName,localPath,toBeReceived;
 
 	public GUITFTP(){
 		super(new BorderLayout());
 
-		//Panel with buttons
-		JPanel panel = new JPanel();
+		//Send Panel
+		JPanel sendPanel = new JPanel();
 		send = new JButton("Send");
 		send.addActionListener(this);
-		panel.add(send);
-
-		/*receive = new JButton("Receive");
-		panel.add(receive);*/
+		sendPanel.add(send);
 
 		selectFile = new JButton("Select a File");
 		selectFile.addActionListener(this);
-		panel.add(selectFile);
+		sendPanel.add(selectFile);
 
-		currentFileName = new JTextArea();
-		panel.add(currentFileName);
+		JTextArea fileName = new JTextArea("Current selected file : ");
+		fileName.setEditable(false);
+		sendPanel.add(fileName);
+
+		currentFileName = new JTextArea("No current file");
+		currentFileName.setEditable(false);
+		sendPanel.add(currentFileName);
+
+
+
+		//Receive panel
+		JPanel receivePanel = new JPanel();
+		receive = new JButton("Receive");
+		receive.addActionListener(this);
+		receivePanel.add(receive);
+
+		fileName = new JTextArea("Enter the server file name here :");
+		fileName.setEditable(false);
+		receivePanel.add(fileName);
+
+		toBeReceived = new JTextArea("...",1,20);
+		toBeReceived.setEditable(true);
+		receivePanel.add(toBeReceived);
 
 		fc = new JFileChooser(System.getProperty("user.dir"));
-		add(BorderLayout.NORTH,panel);
+		add(BorderLayout.NORTH,sendPanel);
+		add(BorderLayout.CENTER,receivePanel);
+
 
 		//Panel with logs
 		JPanel logPanel = new JPanel();
-		log = new JTextArea(50,80);
+		log = new JTextArea(30,80);
 		log.setEditable(false);
 		Font consola = Font.getFont("Consola");
 		if(consola!=null)
@@ -56,22 +76,33 @@ public class GUITFTP extends JPanel implements ActionListener {
 			if (returnVal == JFileChooser.APPROVE_OPTION){
 				currentFile = fc.getSelectedFile();
 				System.out.println("Current file selected : "+currentFile.getName());
-				currentFileName.setText("Current file :"+currentFile.getName());
+				currentFileName.setText(currentFile.getName());
 			}
 		}
 		else if(e.getSource() == send){
 			if(currentFile==null){
-				System.out.println("Please select a file first.");
+				System.out.println("Please select a file to be sent first.");
 			}
 			else
 				TFTPClient.sendFile(currentFile.getPath());
+		}
+		else if(e.getSource() == receive){
+			/*if(localPath.getText().length()==0){
+				System.out.println("Please enter a local path first.");
+			}*/
+			if(toBeReceived.getText().length()==0 || toBeReceived.getText().equals("...")){
+				System.out.println("Please enter a file name first.");
+			}
+			else
+				TFTPClient.receiveFile(toBeReceived.getText());
 		}
 	}
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("TFTP Client");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(900, 900);
+		frame.setSize(950, 650);
+		frame.setResizable(false);
 		frame.add( new GUITFTP());
 		//frame.pack();
 		frame.setVisible(true);
